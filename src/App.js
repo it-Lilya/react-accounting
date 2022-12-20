c20e12772e466b55c17d509be3703a5388eaf496
It_Lilya <ihneva@bk.ru>
2022-12-21 01:38:19 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Accouting from './components/accounting/Accounting';
+
+function App() {
+  return (
+    <Accouting />
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// export default function List({lists, onRemove}) {
+//     return (
+//         <ul>
+//             {lists.map((paragraph) => (
+//                 <input key={paragraph.id}>
+//                     {paragraph.name} <button onClick={() => onRemove(paragraph)}>X</button>
+//                 </input>
+//             ))}
+//         </ul>
+//     )
+// }
